c2f4f921c36d32e785c102b9fb7d422fdb7a71f3
Ngahu <ngangangahu@gmail.com>
2021-03-23 22:36:05 +0300

    Create the Tasks and the Task components and also installed react-icons module.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
+import { useState } from 'react'
 import './App.css';
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 function App() {
+
+  const [tasks, setTasks] = useState(
+    [
+      {
+        id: 1,
+        text: "Coach Appointment",
+        day: 'Feb 5th at 14:30',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'Groceries Shopping',
+        day: 'Feb 6th at 13:00',
+        reminder: false
+      },
+      {
+        id: 3,
+        text: 'School meeting',
+        day: 'Feb 7th at 09:40',
+        reminder: true
+      },
+      {
+        id: 4,
+        text: 'Food Shopping',
+        day: 'Feb 8th at 12:40',
+        reminder: false
+      }
+    ]
+  )
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
